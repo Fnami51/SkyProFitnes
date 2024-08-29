0ec1bb7b27cd60e0b02d57c66e600133d9f8706d
Fnami51 <fnm5160@gmail.com>
2024-08-29 14:27:08 +0300

    Изменения после конфликта. Востановление

---
+++ sky-pro-fitnes/src/components/Cards/Level.tsx
@@ -0,0 +1,17 @@
+function Level({ value }: {value: number}) {
+  const heights = [3, 6, 9, 11, 14];
+
+  return (
+    <div className="flex gap-[0.75px] items-end h-[18px] w-[18px] p-[2px]">
+      {heights.map((height, i) => (
+        <div
+          key={i}
+          className={`w-[2.25px] rounded-[50px] ${i < value ? 'bg-[#00C1FF]' : 'bg-[#D9D9D9]'}`}
+          style={{ height: `${height}px` }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Level;
