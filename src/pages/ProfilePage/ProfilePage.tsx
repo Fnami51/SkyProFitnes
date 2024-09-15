efd36eba76e027b940014c52cc677c428f2e771e
Artem <nadtocheev.nadtocheev@gmail.com>
2024-09-15 15:34:13 +0200

    DEV-28 feat: added context to project

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,24 +1,32 @@
 import MyCourseCard from "../../components/Cards/MyCourseCard"
-import Profil from "./Profile"
+import { useAuth } from "../../context/useAuth"
+import Profile from "./Profile"
 
-function ProfilPage() {
-    
+function ProfilePage() {
+    const { user } = useAuth()
+
     return (
-    <>
-        <main className="mt-[60px] mb-[60px]">
-            <h1 className="text-[40px] font-medium leading-[44px] text-left font-roboto mobile:text-[24px] mobile:leading-[26.4px]">
-                Профиль
-            </h1>
-            <Profil name="Sergei" login="serg228" password="qwertyu" url_img=""/>
-            <h1 className="text-[40px] font-medium leading-[44px] text-left font-roboto mobile:text-[24px] mobile:leading-[26.4px]">
-                Мои курсы
-            </h1>
-            <article className='flex flex-wrap gap-[40px] mt-[50px]'>
-                <MyCourseCard name="Йога" day={25} time="25-30" url_img="/images/yoga_small.png" progress={40} />
-            </article>
-        </main>
-    </>
-    )
+		<>
+			<main className='mt-[60px] mb-[60px]'>
+				<h1 className='text-[40px] font-medium leading-[44px] text-left font-roboto mobile:text-[24px] mobile:leading-[26.4px]'>
+					Профиль
+				</h1>
+				{user && <Profile />}
+				<h1 className='text-[40px] font-medium leading-[44px] text-left font-roboto mobile:text-[24px] mobile:leading-[26.4px]'>
+					Мои курсы
+				</h1>
+				<article className='flex flex-wrap gap-[40px] mt-[50px]'>
+					<MyCourseCard
+						name='Йога'
+						day={25}
+						time='25-30'
+						url_img='/images/yoga_small.png'
+						progress={40}
+					/>
+				</article>
+			</main>
+		</>
+	)
   }
   
-  export default ProfilPage
+  export default ProfilePage
