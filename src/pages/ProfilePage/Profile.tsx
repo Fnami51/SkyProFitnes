5fc8e79005f5726cf28fec142d107e09dde0e5dd
Artem <nadtocheev.nadtocheev@gmail.com>
2024-09-21 17:20:33 +0300

    fixed npm run build

--- src/pages/ProfilePage/Profile.tsx
+++ src/pages/ProfilePage/Profile.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import Modal from '../../components/Modal'
 import { useAuth } from '../../hooks/useAuth'
-import { User } from '../../types/interfaces'
 
 function Profile() {
   const { user, logout, updateUser } = useAuth()
